e39d36d6e16f514642604e6c1d07a3c1a534d642
tjshee39 <gksh054@naver.com>
2023-04-07 13:16:21 +0900

    init project

---
+++ src/main/java/com/sunny/springMVC/main.java
@@ -0,0 +1,26 @@
+package com.sunny.springMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class main {
+    @RequestMapping(value="/")
+    public String index(Model model){
+        model.addAttribute("test", "테스트");
+        return "index";
+    }
+
+    @RequestMapping(value="/json", produces = "application/json")
+    @ResponseBody
+    public Object json(){
+        Map<String, String> m = new HashMap<>();
+        m.put("test", "json test");
+        return m;
+    }
+}
